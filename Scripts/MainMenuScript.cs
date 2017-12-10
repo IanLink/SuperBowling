e96f4421487f00a57cac1a1f8a75d9b4e8c46895
IanLink <ian_link@hotmail.com>
2017-12-10 00:50:08 -0200

    Adicionado save de placar

--- Scripts/MainMenuScript.cs
+++ Scripts/MainMenuScript.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour {
+
+	public Text[] scores;
+
+	void Start(){
+		for (int i = 0; i < scores.Length; i++) {
+			scores[i].text = PlayerPrefs.GetInt ("Level" + (i + 1), 0) + "/10";
+			Debug.Log ("Level" + (i + 1));
+			//Debug.Log (PlayerPrefs.GetInt ("Level" + i + 1, 0));
+		}
+	}
 
 	public void LoadLevel(int i){
 		SceneManager.LoadScene (i);
